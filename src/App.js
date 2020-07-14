f722dc89005698e7305166f6c3872332b778683c
fyang1024 <fyang1024@gmail.com>
2020-07-15 00:05:10 +1000

    Set up initial components structure

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './app.css';
-import logo from './logo.png';
-
-export default function App() {
-  return (
-    <img src={logo} alt={'Dark DEX'} className={'center'} style={{marginTop: 43}}/>
-  );
-}
---
+++ src/components/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './app.css';
+import logo from './logo.png';
+
+const App = () => {
+  return <img src={logo} alt={'Dark DEX'} className={'center'} style={{ marginTop: 43 }}/>;
+};
+
+export default App;
