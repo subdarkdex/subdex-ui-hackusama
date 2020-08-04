cc22b99ee45cc415aafd5ffd61bf4052765ff45f
herryho <herry.heyi@gmail.com>
2020-08-04 23:37:19 +0800

    node component

--- src/components/Node/index.js
+++ src/components/Node/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import useSubstrate from '../../hooks/useSubstrate';
+import './node.css';
 
-export default function Node () {
-  return <div>Placeholder</div>;
+export default function Node() {
+
+  const { socket } = useSubstrate();
+
+  return <div className='nodeContainer'>
+    {socket}
+  </div>;
 }
