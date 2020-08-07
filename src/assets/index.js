c2a1a34b0243a9ef8f602bd32fd869bf634ad9ee
fyang1024 <fyang1024@gmail.com>
2020-08-07 21:39:09 +1000

    Update asset ids

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,9 @@ import ksm from './ksm.png';
 import btc from './btc.png';
 import edg from './edg.png';
 
-const KSM_ASSET_ID = 1;
-const BTC_ASSET_ID = 2;
-const EDG_ASSET_ID = 3;
+const KSM_ASSET_ID = 0;
+const BTC_ASSET_ID = 1;
+const EDG_ASSET_ID = 2;
 
 const assets = [
   {
